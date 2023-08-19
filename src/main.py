5405811ff6a41d3964dd45e524e0638554cf44b9
kiri11-mi1 <kirill.mil.0310@mail.ru>
2023-08-19 15:00:20 +0700

    init script

---
+++ src/main.py
@@ -0,0 +1,21 @@
+from aiogram import Bot, Dispatcher, types, executor
+import settings
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(levelname)s] -  %(name)s - %(filename)s: %(lineno)d - %(message)s"
+)
+
+credentials = settings.Credentials()
+bot = Bot(token=credentials.TELEGRAM_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    await bot.send_message(message.chat.id, 'Hello World')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)

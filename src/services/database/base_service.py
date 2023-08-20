ad23bde1154a4bf65c7c615cd3bba147b77c8565
kiri11-mi1 <kirill.mil.0310@mail.ru>
2023-08-20 20:29:17 +0700

    fix typing

--- src/services/database/base_service.py
+++ src/services/database/base_service.py
@@ -1,4 +1,4 @@
-from typing import Protocol
+from typing import Any, Protocol
 
 from pydantic import UUID4
 
@@ -14,8 +14,8 @@ class BaseDatabaseService(Protocol):
     def get_program_loyalty(self, program_id: UUID4) -> ProgramLoyalty:
         """Получить программу лояльности"""
 
-    def update_program_loyalty(self, program_id: UUID4, updated_data: dict) -> ProgramLoyalty:
+    def update_program_loyalty(self, program_id: UUID4, updated_data: dict[str, Any]) -> ProgramLoyalty:
         """Обновление программы лояльности"""
 
-    def create_program_loyalty(self, created_data: dict) -> ProgramLoyalty:
+    def create_program_loyalty(self, created_data: dict[str, Any]) -> ProgramLoyalty:
         """Создание программы лояльности"""

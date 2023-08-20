8707ddfd53cc6125b9552ed1e80ab226b47a96f2
kiri11-mi1 <kirill.mil.0310@mail.ru>
2023-08-20 20:21:32 +0700

    обновил коммент

--- src/services/database/base_service.py
+++ src/services/database/base_service.py
@@ -6,7 +6,7 @@ from .schemas import ProgramLoyalty, User
 
 
 class BaseDatabaseService(Protocol):
-    """Сервис базы данных."""
+    """Базовый сервис базы данных."""
 
     def get_or_create_user(self, chat_id: str, username: str, first_name: str, last_name: str) -> User:
         """Получить или создать пользователя."""

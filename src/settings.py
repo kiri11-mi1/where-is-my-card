44101705317399359a06429d5dbe90608f662842
kiri11-mi1 <kirill.mil.0310@mail.ru>
2023-08-26 20:53:31 +0700

    новые переменные окружения

--- src/settings.py
+++ src/settings.py
@@ -3,3 +3,6 @@ from pydantic_settings import BaseSettings
 
 class Credentials(BaseSettings):
     TELEGRAM_TOKEN: str
+    DB_URL: str  # TODO: добавить в .dockerenv
+    DB_PASSWORD: str  # TODO: добавить в .dockerenv
+    DB_USER: str  # TODO: добавить в .dockerenv

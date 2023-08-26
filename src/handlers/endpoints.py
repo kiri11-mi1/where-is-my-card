b3fa9f97de90318e1ccc96a913d8d084c7480af8
kiri11-mi1 <kirill.mil.0310@mail.ru>
2023-08-26 22:11:43 +0700

    переобозвал метод

---
+++ src/handlers/endpoints.py
@@ -0,0 +1,14 @@
+from aiogram import types
+from services.database import BaseDatabaseService
+
+from .replies import GREETING
+
+
+async def start(db_service: BaseDatabaseService, message: types.Message) -> None:
+    user = db_service.get_or_create_user(
+        message.chat.id,
+        message.from_user.username,
+        message.from_user.first_name,
+        message.from_user.last_name,
+    )
+    await message.answer(GREETING.format(username=user.username))

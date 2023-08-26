74f5cc282f5818ea592e4141596794034d7bd83c
kiri11-mi1 <kirill.mil.0310@mail.ru>
2023-08-26 22:12:49 +0700

    добавил переменные в докеренв

--- src/settings.py
+++ src/settings.py
@@ -3,6 +3,6 @@ from pydantic_settings import BaseSettings
 
 class Credentials(BaseSettings):
     TELEGRAM_TOKEN: str
-    DB_URL: str  # TODO: добавить в .dockerenv
-    DB_PASSWORD: str  # TODO: добавить в .dockerenv
-    DB_USER: str  # TODO: добавить в .dockerenv
+    DB_URL: str
+    DB_PASSWORD: str
+    DB_USER: str

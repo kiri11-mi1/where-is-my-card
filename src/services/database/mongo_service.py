9b78328a13c0deb993991a3857fa33fa26000ab5
kiri11-mi1 <kirill.mil.0310@mail.ru>
2023-08-27 16:01:31 +0700

    сервис переделаю для вазимодействия с монгой

--- src/services/database/mongo_service.py
+++ src/services/database/mongo_service.py
@@ -2,9 +2,9 @@ from services.database.base_service import BaseDatabaseService
 from services.database.schemas import User
 
 
-class PostgresService(BaseDatabaseService):
-    def __init__(self, postgres_url: str) -> None:
-        self._postgres_url = postgres_url
+class MongoService(BaseDatabaseService):
+    def __init__(self, db_url: str) -> None:
+        self._db_url = db_url
 
     def get_or_create_user(self, chat_id: str, username: str, first_name: str, last_name: str) -> User:
         return User(chat_id=chat_id, username=username, first_name=first_name, last_name=last_name)

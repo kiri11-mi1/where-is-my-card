2773e77193d73b2096b6895066e4882249cafc95
kiri11-mi1 <kirill.mil.0310@mail.ru>
2023-08-27 16:02:15 +0700

    новые импорты

--- src/main.py
+++ src/main.py
@@ -4,8 +4,8 @@ from functools import partial
 from aiogram import Bot, Dispatcher, executor
 
 import settings
-from handlers import start
-from services.database import PostgresService
+from handlers import endpoints
+from services.database.mongo_service import MongoService
 
 logging.basicConfig(level=logging.INFO, format="[%(levelname)s] -  %(name)s - %(filename)s: %(lineno)d - %(message)s")
 
@@ -13,10 +13,10 @@ logging.basicConfig(level=logging.INFO, format="[%(levelname)s] -  %(name)s - %(
 credentials = settings.Credentials()
 bot = Bot(token=credentials.TELEGRAM_TOKEN)
 dp = Dispatcher(bot)
-db_service = PostgresService('sss')
+db_service = MongoService('sss')
 
 # Регистрация хендлеров
-dp.register_message_handler(partial(start, db_service), commands=['start'])
+dp.register_message_handler(partial(endpoints.start, db_service), commands=['start'])
 
 
 if __name__ == '__main__':

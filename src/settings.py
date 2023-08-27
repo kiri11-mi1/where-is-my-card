9e82a5e4ae54638fc01cd273b5f36a09ff6cfa8c
kiri11-mi1 <kirill.mil.0310@mail.ru>
2023-08-27 17:02:17 +0700

    креды для pymongo

--- src/settings.py
+++ src/settings.py
@@ -3,6 +3,12 @@ from pydantic_settings import BaseSettings
 
 class Credentials(BaseSettings):
     TELEGRAM_TOKEN: str
-    DB_URL: str
-    DB_PASSWORD: str
-    DB_USER: str
+
+
+class MongoDB(BaseSettings):
+    MONGO_INITDB_ROOT_USERNAME: str
+    MONGO_INITDB_ROOT_PASSWORD: str
+
+    ME_CONFIG_MONGODB_ADMINUSERNAME: str
+    ME_CONFIG_MONGODB_ADMINPASSWORD: str
+    ME_CONFIG_MONGODB_URL: str
